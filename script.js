2a7ed26fd1ec6e6aa4a95b73184526add4de1e5e
Dasheng Bi <dbi@berkeley.edu>
2021-06-15 19:13:45 -0400

    add imgs, update teaching

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 var expands = document.getElementsByClassName("expand");
-var i;
 
 for (i = 0; i < expands.length; i++) {
-  expands[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
+  var elet = expands[i]
+  if (elet.classList.contains("oexpand")) {
+    elet.classList.toggle("active");
+    var content = elet.nextElementSibling;
+    if (content.style.maxHeight) {
       content.style.maxHeight = null;
     } else {
-      content.style.maxHeight = content.scrollHeight + "px";
+      content.style.maxHeight = content.scrollHeight + 100 + "px";
     }
-  });
+  }
+  elet.addEventListener("click", toggle_expandcontent);
+}
+
+function toggle_expandcontent() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + 100 + "px";
+    }
+}
+
+function evans_hover(e) {
+  e.setAttribute("src", "img/evans_clear")
+}
+function evans_nonhover(e) {
+  e.setAttribute("src", "img/evans_fog")
 }
